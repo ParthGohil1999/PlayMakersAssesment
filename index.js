1dcba34217627bf7be9dd9570944a9ad45be1187
PARTH GOHIL <parth.gohil19@gmail.com>
2023-08-13 05:08:04 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,77 @@
+const Jimp = require('jimp'); 
+
+// Function to verify badge image
+async function verifyBadgeImage(inputPath) {
+  try {
+    const image = await Jimp.read(inputPath);
+
+    // Check size
+    if (image.bitmap.width !== 512 || image.bitmap.height !== 512) {
+      return "Image size should be 512x512.";
+    }
+
+    // Check circle mask
+    const centerX = image.bitmap.width / 2;
+    const centerY = image.bitmap.height / 2;
+    const radius = image.bitmap.width / 2;
+    
+    image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+      const dx = x - centerX;
+      const dy = y - centerY;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance > radius && image.bitmap.data[idx + 3] > 0) {
+        return "Non-transparent pixels should be within a circle.";
+      }
+    });
+
+    // You can add color analysis code here to check if the colors give a "happy" feeling
+
+    return "Badge image is valid.";
+  } catch (error) {
+    return "An error occurred: " + error.message;
+  }
+}
+
+// Function to convert image to badge specifications
+async function convertToBadgeImage(inputPath, outputPath) {
+  try {
+    const image = await Jimp.read(inputPath);
+    
+    // Resize image to 512x512
+    image.resize(512, 512);
+
+    // Apply circle mask
+    image.scan(0, 0, image.bitmap.width, image.bitmap.height, (x, y, idx) => {
+      const centerX = image.bitmap.width / 2;
+      const centerY = image.bitmap.height / 2;
+      const radius = image.bitmap.width / 2;
+      const dx = x - centerX;
+      const dy = y - centerY;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+
+      if (distance > radius) {
+        image.bitmap.data[idx + 3] = 0; // Set alpha channel to 0 for pixels outside the circle
+      }
+    });
+
+    // You can add color manipulation code here to give a "happy" feeling
+
+    await image.writeAsync(outputPath);
+    return "Image converted to badge specifications.";
+  } catch (error) {
+    return "An error occurred: " + error.message;
+  }
+}
+
+// Usage example
+const inputImagePath = 'test.jpg';
+const outputImagePath = 'badge.png';
+
+verifyBadgeImage(inputImagePath).then(result => {
+  console.log(result);
+});
+
+convertToBadgeImage(inputImagePath, outputImagePath).then(result => {
+  console.log(result);
+});
